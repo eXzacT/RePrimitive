137caf87152be247d88268875b482cc287d81b3b
Damjan Anđelković <damjan.andelkovic1@gmail.com>
2022-04-08 01:58:06 +0200

    Moved panel to be a separate panel, readded popup and fixed lag

--- prefs.py
+++ prefs.py
@@ -1,45 +1,7 @@
-from bpy.types import AddonPreferences, Panel
+from bpy.types import AddonPreferences
 from bpy.props import BoolProperty, IntProperty
-from bpy.app import version
 from . import addon_updater_ops
 import rna_keymap_ui
-
-
-class DemoUpdaterPanel(Panel):
-    """Panel to demo popup notice and ignoring functionality"""
-    bl_label = "Updater Demo Panel"
-    bl_idname = "OBJECT_PT_DemoUpdaterPanel_hello"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'TOOLS' if version < (2, 80) else 'UI'
-    bl_context = "objectmode"
-    bl_category = "Tools"
-
-    def draw(self, context):
-        layout = self.layout
-
-        # Call to check for update in background.
-        # Note: built-in checks ensure it runs at most once, and will run in
-        # the background thread, not blocking or hanging blender.
-        # Internally also checks to see if auto-check enabled and if the time
-        # interval has passed.
-        addon_updater_ops.check_for_update_background()
-
-        layout.label(text="Demo Updater Addon")
-        layout.label(text="")
-
-        col = layout.column()
-        col.scale_y = 0.7
-        col.label(text="If an update is ready,")
-        col.label(text="popup triggered by opening")
-        col.label(text="this panel, plus a box ui")
-
-        # Could also use your own custom drawing based on shared variables.
-        if addon_updater_ops.updater.update_ready:
-            layout.label(text="Custom update message", icon="INFO")
-        layout.label(text="")
-
-        # Call built-in function with draw code/checks.
-        addon_updater_ops.update_notice_box_ui(self, context)
 
 
 @addon_updater_ops.make_annotations
--- ui.py
+++ ui.py
@@ -1,25 +1,32 @@
 import bpy
 from bpy.types import Panel
+from bpy.app import version
+from . import addon_updater_ops
 
 
 class RePrimitivePanel(Panel):
     """
-    Panel with 2 buttons, rotate and main reprimitive operator
+    Panel with 2 buttons, rotate and main reprimitive operator,
+    also has the update popup
     """
 
     bl_idname = 'VIEW3D_PT_RePrimitive_Panel'
     bl_label = 'RePrimitive'
     bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = "Tool"
+    bl_context = "objectmode"
+    bl_region_type = 'TOOLS' if version < (2, 80) else 'UI'
+    bl_category = "RePrimitive"
 
-    # if the object exists and the panel is enabled show it
+    # if the panel is enabled show it
     @classmethod
     def poll(cls, context):
-        return context.object is not None and bpy.context.preferences.addons[__package__].preferences.show_reprimitive_panel
+        return bpy.context.preferences.addons[__package__].preferences.show_reprimitive_panel
 
     def draw(self, context):
         layout = self.layout
+
+        # Call to check for update in background.
+        addon_updater_ops.check_for_update_background()
 
         # Rotate button
         row = layout.row()
@@ -30,5 +37,8 @@ class RePrimitivePanel(Panel):
         row = layout.row()
         row.scale_y = 1.6
         row.operator("object.reprimitive", text="RePrimitive")
+
+        # Call built-in function with draw code/checks.
+        addon_updater_ops.update_notice_box_ui(self, context)
 
         return
